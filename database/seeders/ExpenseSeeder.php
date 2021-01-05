d6eb87d8e95117242a1ca36ee18470fdf55145bf
Doğukan Kökce <dogukankokcew@gmail.com>
2021-01-06 01:32:21 +0300

    Fatal errors fixed for real

--- database/seeders/ExpenseSeeder.php
+++ database/seeders/ExpenseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
 
 class ExpenseSeeder extends Seeder
 {
@@ -16,97 +18,97 @@ class ExpenseSeeder extends Seeder
     {
         DB::table('expenses')->insert([
             'id' => 1,
+            'name' => 'water',
         	'monthlyexpense_id' => 1,
         	'amount' => 100,
-        	'date' => 1,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 2,
+            'name' => 'water',
         	'monthlyexpense_id' => 2,
         	'amount' => 100,
-        	'date' => 2,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 3,
+            'name' => 'water',
         	'monthlyexpense_id' => 3,
         	'amount' => 100,
-        	'date' => 3,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 4,
+            'name' => 'water',
         	'monthlyexpense_id' => 4,
         	'amount' => 100,
-        	'date' => 4,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 5,
+            'name' => 'water',
         	'monthlyexpense_id' => 5,
         	'amount' => 100,
-        	'date' => 5,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 6,
+            'name' => 'water',
         	'monthlyexpense_id' => 6,
         	'amount' => 100,
-        	'date' => 6,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 7,
+            'name' => 'water',
         	'monthlyexpense_id' => 7,
         	'amount' => 100,
-        	'date' => 7,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 8,
+            'name' => 'water',
         	'monthlyexpense_id' => 8,
         	'amount' => 100,
-        	'date' => 8,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 9,
+            'name' => 'water',
         	'monthlyexpense_id' => 9,
         	'amount' => 100,
-        	'date' => 9,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 10,
+            'name' => 'water',
         	'monthlyexpense_id' => 10,
         	'amount' => 100,
-        	'date' => 10,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 11,
+            'name' => 'water',
         	'monthlyexpense_id' => 11,
         	'amount' => 100,
-        	'date' => 11,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
         DB::table('expenses')->insert([
             'id' => 12,
+            'name' => 'water',
         	'monthlyexpense_id' => 12,
         	'amount' => 100,
-        	'date' => 12,
         	'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
 			'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
